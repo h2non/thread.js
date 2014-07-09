575055249b464a46b61b0d312b09af77362b096d
Tomas Aparicio <tomas@aparicio.me>
2014-07-10 00:09:57 +0200

    feat: update files

--- test/thread.js
+++ test/thread.js
@@ -4,4 +4,12 @@ describe('Thread', function () {
     expect(window.Worker).to.be.an('object')
   })
 
+  it('should expose the Thread object constructor', function () {
+    expect(window.Thread).to.be.a('function')
+  })
+
+  it('should expose the thread factory', function () {
+    expect(window.thread).to.be.a('function')
+  })
+
 })
