72b6d8e9d3e31c115edac6eb0b3db54136c3e4a6
Tomas Aparicio <tomas@aparicio.me>
2014-09-21 18:20:13 +0200

    chore(test): add karma config

---
+++ karma.conf.js
@@ -0,0 +1,23 @@
+module.exports = function(config) {
+  config.set({
+    files: [
+      'node_modules/chai/chai.js',
+      'thread.js',
+      'test/*.js'
+    ],
+    exclude: [
+      'test/utils.js'
+    ],
+    frameworks: ['mocha'],
+    browsers: [
+      'Chrome',
+      'ChromeCanary',
+      'Firefox',
+      'PhantomJS',
+      'Opera',
+      'Safari'
+    ],
+    reports: ['progress'],
+    singleRun: true
+  })
+}
