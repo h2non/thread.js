26874702c36aadf5dd4edd946c7050ba7e84f819
Tomas Aparicio <tomas@aparicio.me>
2014-11-22 12:56:46 +0100

    refactor(utils)

--- thread.js
+++ thread.js
@@ -670,7 +670,7 @@ _.isObj = function (o) {
 }
 
 _.isArr = function (o) {
-  return (o && isArrayNative ? isArrayNative(o) : toStr.call(o) === '[object Array]') || false
+  return o && (isArrayNative ? isArrayNative(o) : toStr.call(o) === '[object Array]') || false
 }
 
 _.toArr = function (args) {
