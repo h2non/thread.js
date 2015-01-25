34662d8c2f0ef4167680550388465d690cd6d43c
Tomas Aparicio <tomas@aparicio.me>
2015-01-25 01:38:40 +0100

    refactor: globalize window for better compability

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION = 0.1.14
+VERSION = 0.1.15
 BROWSERIFY = node ./node_modules/browserify/bin/cmd.js
 MOCHA = ./node_modules/.bin/mocha
 UGLIFYJS = ./node_modules/.bin/uglifyjs
--- thread.js
+++ thread.js
@@ -1,4 +1,4 @@
-/*! thread.js - v0.1.14 - MIT License - https://github.com/h2non/thread.js */
+/*! thread.js - v0.1.15 - MIT License - https://github.com/h2non/thread.js */
 !function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.thread=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var _ = require('./utils')
 var workerSrc = require('./worker')
@@ -117,7 +117,7 @@ function ThreadFactory(options) {
   return new Thread(options)
 }
 
-ThreadFactory.VERSION = '0.1.14'
+ThreadFactory.VERSION = '0.1.15'
 ThreadFactory.create = ThreadFactory
 ThreadFactory.Task = Thread.Task
 ThreadFactory.Thread = Thread
@@ -782,6 +782,10 @@ function worker() {
     var isArrayNative = Array.isArray
     var queue, origin, scriptsLoad, intervalId = null
     self.addEventListener = self[eventMethod]
+
+    if (typeof window === 'undefined') {
+      self.window = self
+    }
 
     function isObj(o) {
       return o && toStr.call(o) === '[object Object]'
