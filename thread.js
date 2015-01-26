ea7a8c8b990e28d3001439169cf7a56d44742e35
Tomas Aparicio <tomas@aparicio.me>
2015-01-26 20:08:14 +0100

    refactor: remove log

--- thread.js
+++ thread.js
@@ -1,4 +1,4 @@
-/*! thread.js - v0.1.15 - MIT License - https://github.com/h2non/thread.js */
+/*! thread.js - v0.1.16 - MIT License - https://github.com/h2non/thread.js */
 !function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.thread=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var _ = require('./utils')
 var workerSrc = require('./worker')
@@ -137,6 +137,7 @@ module.exports = threadPool
 
 function threadPool(poolSize, thread) {
   var threads = [ thread ]
+
   thread._run = thread.run
   thread._terminate = thread.terminate
 
@@ -197,7 +198,6 @@ function findBestAvailableThread(threads, offset) {
     pending = thread.pending()
     if (pending === 0 || pending < offset) {
       if (thread.terminated) {
-        console.log('Length >', l, i)
         threads.splice(i, 1)
         l -= 1; i -= 1
       } else {
